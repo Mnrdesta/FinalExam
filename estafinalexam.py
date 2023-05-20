7ebc12dbc89812c2d00118ed87fd7e8da0ca0e40
Mnrdesta <134135765+Mnrdesta@users.noreply.github.com>
2023-05-20 06:21:46 -0400

    estafinalexam.py

--- estafinalexam.py
+++ estafinalexam.py
@@ -1,30 +1,35 @@
 import streamlit as st
 import tensorflow as tf
+from PIL import Image, ImageOps
+import numpy as np
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-  model=tf.keras.models.load_model('Finals.h5')
-  return model
-st.write("""
-# Digits Classifier"""
-)
-file=st.file_uploader("Choose digit photo from computer",type=["jpg","png"])
-import cv2
-from PIL import Image,ImageOps
-import numpy as np
-def import_and_predict(image_data,model):
-    size=(64,64)
-    image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
-    img=np.asarray(image)
-    img_reshape=img[np.newaxis,...]
-    prediction=model.predict(img_reshape)
+    model = tf.keras.models.load_model('Finals.h5')
+    return model
+
+def import_and_predict(image_data, model):
+    size = (64, 64)
+    image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+    img = np.asarray(image)
+    img_reshape = img[np.newaxis, ...]
+    prediction = model.predict(img_reshape)
     return prediction
+
+st.write("""
+# Digits Classifier
+""")
+
+file = st.file_uploader("Choose digit photo from computer", type=["jpg", "png"])
+
 if file is None:
     st.text("Please upload an image file")
 else:
-    image=Image.open(file)
-    st.image(image,use_column_width=True)
-    prediction=import_and_predict(image,model)
-    class_names=['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    string="OUTPUT : "+class_names[np.argmax(prediction)]
-    st.success(string)
+    model = load_model()
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
+    prediction = import_and_predict(image, model)
+    class_names = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    predicted_class = np.argmax(prediction)
+    predicted_class_name = class_names[predicted_class]
+    st.success("OUTPUT: " + predicted_class_name)
