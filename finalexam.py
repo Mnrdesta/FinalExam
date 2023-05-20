4e3cb149921194bb8dd326f7cb1780dafe5a4072
Mnrdesta <134135765+Mnrdesta@users.noreply.github.com>
2023-05-20 06:44:20 -0400

    finalexam.py

--- finalexam.py
+++ finalexam.py
@@ -4,8 +4,6 @@ from PIL import Image, ImageOps
 import numpy as np
 
 @st.cache(allow_output_mutation=True, hash_funcs={tf.keras.models.Model: id})
-def load_model():
-    model = tf.keras.models.load_model('Finals.h5')
     return model
 
 def import_and_predict(image_data, model):
@@ -25,6 +23,7 @@ file = st.file_uploader("Choose digit photo from computer", type=["jpg", "png"])
 if file is None:
     st.text("Please upload an image file")
 else:
+    model = tf.keras.models.load_model('Finals.h5')
     image = Image.open(file)
     st.image(image, use_column_width=True)
     prediction = import_and_predict(image, model)
